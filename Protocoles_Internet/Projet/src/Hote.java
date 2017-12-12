16326fc112da526748b50359d330565cf1fa99dc
marchk <M19_95@hotmail.fr>
2017-12-12 01:43:08 +0100

    Update Hote.java

--- Protocoles_Internet/Projet/src/Hote.java
+++ Protocoles_Internet/Projet/src/Hote.java
@@ -13,13 +13,19 @@ public class Hote extends Conv implements Runnable{
 	
 	public void run(){
 		try{
+			System.out.println("Hote en attente sur le port "+this.port);
 			ServerSocket server = new ServerSocket(this.port);
 			while(true){
+				System.out.println("Connexion en cours...");
 				Socket s=server.accept();
 				this.socket = s;
 				Conversation c = new Conversation(this);
 				Thread t = new Thread(c);
 				t.start();
+				t.join();
+				s.close();
+				server.close();
+				break;
 			}
 		}
 		catch(Exception e){
